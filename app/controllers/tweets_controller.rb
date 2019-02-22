30938ee1a7b47c9234b8d0349473056d768c823b
Jason Mei <jason.mei403@gmail.com>
2019-02-22 02:56:00 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -20,6 +20,7 @@ class TweetsController < ApplicationController
     @tweet = Tweet.create(params[:tweet])
     @tweet.user_id = session[:user_id]
     @tweet.save
+    binding.pry
     redirect "/tweets/#{@tweet.id}"
   end
 
